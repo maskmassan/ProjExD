bbc60cbf535e45e93e0202620df2528f243a2828
masaki <c0a210722e@edu.teu.ac.jp>
2022-06-14 19:47:23 +0900

    修正しました #3

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,7 +1,7 @@
 import random
 def main():
     NUM_OF_TRIALS = 5
-    NUM_OF_ALL_MISS = 2
+    kekkanmozi = 2
     zyoutai=0
     for i in range(NUM_OF_TRIALS):
         seikai = shutudai()
@@ -10,7 +10,7 @@ def main():
             break
 
 
-def shutudai():
+def shutudai():#対象文字と欠陥文字を表示する
     al=[chr(c+65) for c in range(26)]
     tenlist = random.sample(al,10)
     print("対象文字:")
@@ -21,7 +21,7 @@ def shutudai():
 
 
 
-def kaitou(seikai):
+def kaitou(seikai):#欠陥文字と入力文字があっているか確認する
     ans = int(input("欠陥文字はいくつあるでしょうか"))
     if ans!=2:
         print("不正解")
@@ -29,18 +29,18 @@ def kaitou(seikai):
     if ans ==2:
         print("正解です")
         print("次に具体的に欠陥文字を入れてください")
-        for i in range(int(NUM_OF_TRIALS)):
+        for i in range(2):
             c= input(f"{i+1}の目の文字を入れてください:")
             if c not in tenlist:
-                print("正解です")
+                print("正解です")  
+                continue
 
             else:
-                print("不正解です")
-                zyoutai==1
+                print("不正解です")#aaaa
+                zyoutai==1#a
                 
 
 
 
 if __name__ == "__main__":
     main()
-    print("終了")
