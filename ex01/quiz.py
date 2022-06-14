c559973cd4ee21e1c9c996ba93ef53aea9be8d19
masaki <c0a210722e@edu.teu.ac.jp>
2022-06-14 16:19:57 +0900

    出来た

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -7,15 +7,15 @@ def shukudai():
     qas = [
         {"q":"サザエの旦那の名前は","a":["マスオ","ますお"]},
         {"q":"カツオの妹の名前は","a":["わかめ","ワカメ"]}
-    ]
+        ]
     print("問題")
     r=random.randint(0,1)
-    print(qas[r["q"]])
+    print(qas[r]["q"])
     return  qas[r]["a"]
 
 def kaitou(seikai):
     ans =input("答えるんだ")
-    if ans in seiseki:
+    if ans in seikai:
         print("正解")
     else:
         print("失敗")
