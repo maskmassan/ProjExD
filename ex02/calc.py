ccc2e7d6f9c571ad6d2a24b8615c3bb6a7977b3f
masaki <c0a210722e@edu.teu.ac.jp>
2022-06-21 15:32:31 +0900

    足し算まで出来

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,28 +1,29 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def button_click(event):
+def button_click(event): #ボタンがクリックされた時の動作
     btn = event.widget
     tex = btn["text"]
     #tkm.showinfo(f"{tex}がクリックされました")
-    if tex=="=":
+    if tex=="=":#　=を押したとき
          eqn = entry.get()
          ans = eval(eqn)
          entry.delete(0, tk.END)
          entry.insert(tk.END,ans)
+
     else:
         entry.insert(tk.END,tex)
 
 if __name__=="__main__":
 
     root = tk.Tk()
-    root.geometry("300x600")
+    root.geometry("300x600")#300×600の枠
     r,c=1,0
 
     entry = tk.Entry(root, justify="right",width =10,font=("Times New Roman",40))
     entry.grid(row=0,column=0,columnspan=3)
 
-    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):#表示されるコマンド
         btn = tk.Button(root,text=f"{num}",width =4,height=2,font=("Times New Roman",25))
         btn.grid(row = r , column=c)
         btn.bind("<1>",button_click)
