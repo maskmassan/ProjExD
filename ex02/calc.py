bce9576a0bae94b8da50ab0aae45a4d2d10c5f5e
masaki <c0a210722e@edu.teu.ac.jp>
2022-06-21 14:27:57 +0900

    数字表せた

---
+++ ex02/calc.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+if __name__=="__main__":
+
+    root = tk.Tk()
+    root.geometry("300x500")
+    r,c=0,0
+    for i in range(9,-1,-1):
+        btn = tk.Button(root,text=f"{i}",width =4,height=2,font=("Times New Roman",30))
+        btn.grid(row = r , column=c)
+        c+=1
+        if (i-1)%3==0:
+            r+=1
+            c=0
+        
+
+
+
+
+root.mainloop()
